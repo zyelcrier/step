b259342a5d1193c148bf5da30554276b6a5a9540
Zyel Crier <zyel.crier@gmail.com>
2020-05-26 12:24:40 -0500

    add first profile image made changess to stylesheet and added a movieline function

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -19,10 +19,24 @@ function addRandomGreeting() {
   const greetings =
       ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
 
+
   // Pick a random greeting.
   const greeting = greetings[Math.floor(Math.random() * greetings.length)];
+
 
   // Add it to the page.
   const greetingContainer = document.getElementById('greeting-container');
   greetingContainer.innerText = greeting;
+}
+
+function addRandomline() {
+    const lines=
+        ['Im a peacock you gotta let me fly','just do it','crewmen','clap your hands if you believe', 'FIRE EVERYTHING!','You will be obliterated']
+  // Pick a random greeting.
+
+const movieline = lines[Math.floor(Math.random() * lines.length)];
+
+  // Add it to the page.
+  const lineContainer = document.getElementById('line-container');
+  lineContainer.innerText = movieline;
 }
