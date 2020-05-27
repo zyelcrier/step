16ab1948c8915a93e52730dbfce2c1a730ec2132
Zyel Crier <zyel.crier@gmail.com>
2020-05-27 13:09:08 -0500

    Fixed spacing in style.css and in script.js

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -21,7 +21,7 @@ function addRandomGreeting() {
 
   // Pick a random greeting.
   const greeting = greetings[Math.floor(Math.random() * greetings.length)];
-
+  
   // Add it to the page.
   const greetingContainer = document.getElementById('greeting-container');
   greetingContainer.innerText = greeting;
