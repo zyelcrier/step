f80b6864648791f102bde4f68c9510b233bdf209
Zyel Crier <zyel.crier@gmail.com>
2020-06-05 17:14:33 +0000

    New Servlet

---
+++ portfolio/src/main/java/com/google/sps/servlets/Delete.java
@@ -0,0 +1,37 @@
+package com.google.sps.servlets;
+
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.KeyFactory;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.SortDirection;
+import com.google.gson.Gson;
+import com.google.sps.data.Comment;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/** Servlet that returns some example content. TODO: modify this file to handle comments data */
+@WebServlet("/delete-data")
+public class Delete extends HttpServlet {
+    
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+        long id = Long.parseLong(request.getParameter("id"));
+
+        Key commentEntityKey = KeyFactory.createKey("Comment", id);
+        System.out.print(commentEntityKey);
+        datastore.delete(commentEntityKey);
+        
+        response.sendRedirect("/index.html");
+    }
+}
