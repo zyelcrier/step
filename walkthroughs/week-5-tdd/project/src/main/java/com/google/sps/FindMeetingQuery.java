b7c6821db58faf4f23b130339b5f2170f7f2c01e
Zyel Crier <zyel.crier@gmail.com>
2020-06-18 17:54:35 +0000

    Initial TDD Finish

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -15,9 +15,84 @@
 package com.google.sps;
 
 import java.util.Collection;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 public final class FindMeetingQuery {
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    throw new UnsupportedOperationException("TODO: Implement this method.");
+    Collection<TimeRange> result = new ArrayList<TimeRange>(); 
+
+    long meetingRequestDuration = request.getDuration();
+    Collection<Event> eventsWithRequestedAttendees = getEventsWithRequestedAttendees(events, request.getAttendees());
+    Collection<TimeRange> availabeRanges = new ArrayList<>();
+
+    if(events.size()==0 && meetingRequestDuration<=TimeRange.WHOLE_DAY.end()){ 
+      return result = Arrays.asList(TimeRange.WHOLE_DAY);
+    }
+    else{
+      availabeRanges = findAllAvailableTimeRanges(eventsWithRequestedAttendees);
+      result = findViableTimeRangesForRequest(availabeRanges, meetingRequestDuration);
+    }
+
+    return result;
+  }
+
+  Collection<Event> getEventsWithRequestedAttendees(Collection<Event> events, Collection<String> meetingRequestAttendees){
+    Collection<Event> result = new ArrayList<Event>();
+    for(Event event: events){
+      for(String attendee : meetingRequestAttendees){
+        if(event.getAttendees().contains(attendee)){
+        result.add(event);
+        }
+      }    
+    } 
+    return result;
+  }
+
+  Collection<TimeRange> findAllAvailableTimeRanges(Collection<Event> eventsWithRequestedAttendees){
+    Collection<TimeRange> availabeRanges = new ArrayList<TimeRange>();
+    int min = -1;
+    for(int minute=TimeRange.WHOLE_DAY.start(); minute<TimeRange.WHOLE_DAY.end()+1; minute++){
+      boolean isMinuteInEvent = false;
+      for(Event event: eventsWithRequestedAttendees){
+        isMinuteInEvent = event.getWhen().contains(minute); //if events range does or does not include current minute
+        if(isMinuteInEvent){
+          break;
+        }
+      }
+      if(isMinuteInEvent){//if current minute is within range of current event in loop
+        if(min!=-1){// if min has been set
+          TimeRange foundMeetingRange = TimeRange.fromStartDuration(min,minute-min);
+          availabeRanges.add(foundMeetingRange);
+          min = -1;
+        }
+      }
+      else{//Establish a start of the time range
+        if(min==-1){
+          min = minute;
+        }
+        else{
+          if(minute==TimeRange.WHOLE_DAY.end()){
+            TimeRange foundMeetingRange = TimeRange.fromStartDuration(min,minute-min);
+            availabeRanges.add(foundMeetingRange);
+          }
+        }
+      }
+    }
+    return availabeRanges;
+  }
+
+  Collection<TimeRange> findViableTimeRangesForRequest(Collection<TimeRange> availabeRanges , long meetingRequestDuration){
+    Collection<TimeRange> result = new ArrayList<TimeRange>();    
+    for(TimeRange available: availabeRanges){
+      if(available.duration() >= meetingRequestDuration){
+        result.add(available);    
+      }
+    }
+    return result;
   }
 }
